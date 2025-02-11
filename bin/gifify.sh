2cc71c1c35ec21eff56ee7128ef034c7055fcfc7
Johan Palmfjord <johan@palmenhq.dev>
2025-02-11 09:39:16 +0100

    Add video -> gif maker script

---
+++ bin/gifify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sext -xeuo pipefail
+
+input_file="${1}"
+output_file="${2}"
+width=${3:-"1200"}
+
+ffmpeg -i "${input_file}" -filter_complex "[0:v] fps=10,scale=${width}:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" "${output_file}"
